d4b7417f69be24bcfdbe4027726845a946083686
lucadani7 <79052990+lucadani7@users.noreply.github.com>
2025-05-20 07:01:10 +0300

    app.py file written

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,18 @@
 from flask import Flask
+from flask_cors import CORS
+
+from backend.routes.file_routes import file_routes
 
 app = Flask(__name__)
+CORS(app)  # allowing API access from frontend
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home():
+    return "Backend is running!"
 
+
+app.register_blueprint(file_routes)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='127.0.0.1', port=5000)
